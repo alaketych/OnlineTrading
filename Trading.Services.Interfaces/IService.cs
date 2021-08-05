fc9f206e18d7da61d1821006f4891d0a29a2e5d1
alaketych <alaketych@ukr.net>
2021-08-05 13:21:26 +0300

    add generic interface

---
+++ Trading.Services.Interfaces/IService.cs
@@ -0,0 +1,20 @@
+﻿namespace Trading.Services.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Trading.Domain.Interfaces;
+
+    public interface IService<T> where T : class, IEntity
+    {
+        Task<IList<T>> GetAllAsync();
+
+        Task<T> GetIdAsync(int id);
+
+        Task<T> AddAsync(T item);
+
+        Task<T> UpdateAsync(T id);
+    }
+}
