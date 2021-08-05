1ad45375ac3927da1f4e880244b3a0da6eb93f0a
alaketych <alaketych@ukr.net>
2021-08-05 13:22:35 +0300

    add user repository

---
+++ Trading.Infrastucture.Data/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace Trading.Infrastucture.Data.Repositories
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+	using System.Threading.Tasks;
+	using Trading.Domain.Entities;
+
+	public class UserRepository : Repository<User, DataContext>
+	{
+		private readonly DataContext _context;
+
+		public UserRepository(DataContext context) : base(context)
+		{
+			_context = context;
+		}
+	}
+}
